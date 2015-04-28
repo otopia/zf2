91b8f426ff7112b94d108c0c8fc36b1d53b88ee8
otopia <rezvany@gmail.com>
2015-04-28 17:17:29 +0430

    new module and new controller in new module

--- 01/module/Register/src/Register/Controller/IndexController.php
+++ 01/module/Register/src/Register/Controller/IndexController.php
@@ -2,21 +2,20 @@
 /**
  * Zend Framework (http://framework.zend.com/)
  *
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @link      http://github.com/zendframework/Register for the canonical source repository
  * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
-namespace Application\Controller;
+namespace Register\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
-use Zend\View\Model\ViewModel;
 
 class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
-        $bb=array('value'=>'salam');
-        return $bb;
+        return array();
     }
+
 }
---
+++ 01/module/Register/src/Register/Controller/LoginController.php
@@ -0,0 +1,26 @@
+<?php
+namespace Register\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+
+/**
+ * LoginController
+ *
+ * @author
+ *
+ * @version
+ *
+ */
+class LoginController extends AbstractActionController
+{
+
+    /**
+     * The default action - show the home page
+     */
+    public function loginAction()
+    {
+        // TODO Auto-generated LoginController::indexAction() default action
+        return array();
+    }
+}
